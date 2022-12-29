78d5d8bafa15dcc0ba682bf50f7224cf4e529ddd
AnixDrone <tomislavignjatov@rocketmail.com>
2022-12-29 18:17:12 +0100

    add train script for train_vae

---
+++ train_script.sh
@@ -0,0 +1,7 @@
+python3 train_vae.py -epochs 100 -batch_size 128 -latent_dim 1024 -train -test
+python3 train_vae.py -epochs 200 -batch_size 128 -latent_dim 1024 -train -test
+python3 train_vae.py -epochs 100 -batch_size 64 -latent_dim 1024 -train -test
+python3 train_vae.py -epochs 100 -batch_size 32 -latent_dim 1024 -train -test
+python3 train_vae.py -epochs 100 -batch_size 32 -latent_dim 2048 -train -test
+python3 train_vae.py -epochs 100 -batch_size 32 -latent_dim 512 -train -test
+python3 train_vae.py -epochs 100 -batch_size 32 -latent_dim 64 -train -test
